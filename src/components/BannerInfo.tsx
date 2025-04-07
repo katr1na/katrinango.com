faceed27aa2eeff9bd7a965913e4d55d11fe6043
Katrina Ngo <katrinango3388@gmail.com>
2025-04-07 10:35:14 -0600

    update profile pic

--- src/components/BannerInfo.tsx
+++ src/components/BannerInfo.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import profileIcon from '../assets/profile-pic.jpg';
+import profileIcon from '../assets/profile-pic.jpeg';
 import '../styles.css';
 
 const BannerInfo = () => {
